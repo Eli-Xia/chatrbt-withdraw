b53a1f5e9b9d6ea5c5c5bd3e06307aecb6fdff82
xiaxin <244011876@qq.com>
2018-05-03 16:59:13 +0800

    今日任务

--- src/net/monkeystudio/chatpet/controller/ChatPetBaseController.java
+++ src/net/monkeystudio/chatpet/controller/ChatPetBaseController.java
@@ -7,8 +7,9 @@ import net.monkeystudio.base.BaseController;
  * @author xiaxin
  */
 public class ChatPetBaseController extends BaseController {
+    public static final String SESSION_ATTR_NAME_CHATPET_USERID = "SESSION_ATTR_NAME_CHATPET_USERID";
     @Override
     protected String getSessionUserIdAttrName() {
-       return Constants.SESSION_ATTR_NAME_CHATPET_USERID;
+       return SESSION_ATTR_NAME_CHATPET_USERID;
     }
 }
