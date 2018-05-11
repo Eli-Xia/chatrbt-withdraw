7253062ba5f69adcc09392f5800c45f44ed37be1
linhongbin <312763889@qq.com>
2018-05-11 15:17:29 +0800

    完成宠物外观的api接口

---
+++ src/net/monkeystudio/chatrbtw/service/bean/chatpetappearence/Appearance.java
@@ -0,0 +1,26 @@
+package net.monkeystudio.chatrbtw.service.bean.chatpetappearence;
+
+/**
+ * Created by bint on 2018/5/11.
+ */
+public class Appearance {
+
+    private Integer chatPetType;
+    private Object object;
+
+    public Integer getChatPetType() {
+        return chatPetType;
+    }
+
+    public void setChatPetType(Integer chatPetType) {
+        this.chatPetType = chatPetType;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
