ebb65bc55c71a10789cbcaa83de12ee9007361ae
xiaxin <244011876@qq.com>
2018-05-22 14:56:50 +0800

    任务

--- src/net/monkeystudio/chatpet/controller/ChatPetController.java
+++ src/net/monkeystudio/chatpet/controller/ChatPetController.java
@@ -40,12 +40,12 @@ public class ChatPetController extends ChatPetBaseController{
     @ResponseBody
     @RequestMapping(value = "/info", method = RequestMethod.POST)
     public RespBase getAdClickLogList(HttpServletRequest request,HttpServletResponse response){
-        Integer fanId = getUserId();
+        /*Integer fanId = getUserId();
 
         if(fanId == null){
             return respHelper.nologin();
-        }
-        //Integer fanId = 104;
+        }*/
+        Integer fanId = 104;
 
         ChatPetInfo chatPetInfo = chatPetService.getInfoByWxFanId(fanId);
 
