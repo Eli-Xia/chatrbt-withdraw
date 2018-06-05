c2582ec39f58288d443efdd12f180a49f5517ae4
xiaxin <244011876@qq.com>
2018-06-05 17:58:32 +0800

    修改资讯任务超出次数的问题

--- src/net/monkeystudio/chatrbtw/service/AdPushService.java
+++ src/net/monkeystudio/chatrbtw/service/AdPushService.java
@@ -8,6 +8,7 @@ import net.monkeystudio.chatrbtw.entity.Ad;
 import net.monkeystudio.chatrbtw.entity.AdPushLog;
 import net.monkeystudio.chatrbtw.entity.WxFan;
 import net.monkeystudio.chatrbtw.sdk.wx.WxCustomerHelper;
+import net.monkeystudio.chatrbtw.service.bean.chatpetmission.DispatchMissionParam;
 import net.monkeystudio.wx.service.WxPubService;
 import net.monkeystudio.wx.service.WxTextMessageHandler;
 import net.monkeystudio.wx.vo.customerservice.CustomerNewsItem;
@@ -52,6 +53,9 @@ public class AdPushService {
     private CfgService cfgService;
 
     private static String AD_COUNT_SYS_API =  null;
+
+    @Autowired
+    private ChatPetService chatPetService;
 
     @PostConstruct
     public void init(){
@@ -208,9 +212,16 @@ public class AdPushService {
         Log.d("===============陪聊宠随机任务触发 检查参数 adId = {?} , wxFanId = {?} =================",ad.getId().toString(),wxFanId.toString());
         //陪聊宠随机任务触发
         //chatPetMissionPoolService.updateMissionWhenPushChatPetAd(ad.getId(),wxFanId);
-        if(adService.AD_PUSH_TYPE_CHAT_PET.equals(ad.getPushType())){
+        /*if(adService.AD_PUSH_TYPE_CHAT_PET.equals(ad.getPushType())){
             chatPetMissionPoolService.saveMissionRecordWhenPushChatPetAd(ad.getId(),wxFanId);
-        }
+        }*/
+        DispatchMissionParam param = new DispatchMissionParam();
+
+        param.setChatPetId(chatPetService.getChatPetIdByFans(wxFan.getWxPubOriginId(),wxFan.getWxFanOpenId()));
+        param.setMissionCode(ChatPetMissionEnumService.SEARCH_NEWS_MISSION_CODE);
+        param.setAdId(ad.getId());
+
+        chatPetMissionPoolService.dispatchMission(param);
 
         //记录广告推送日志
         AdPushLog adPushLog = new AdPushLog();
