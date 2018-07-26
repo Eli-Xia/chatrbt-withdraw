e5d418dda432afcbf59c5cf02876b6ed34fd355a
xiaxin <244011876@qq.com>
2018-07-26 16:07:15 +0800

    小改动

--- src/net/monkeystudio/chatpet/controller/BackdoorController.java
+++ src/net/monkeystudio/chatpet/controller/BackdoorController.java
@@ -1,22 +1,13 @@
 
 package net.monkeystudio.chatpet.controller;
 
-import net.monkeystudio.base.controller.bean.RespBase;
 import net.monkeystudio.base.utils.RespHelper;
-import net.monkeystudio.chatrbtw.entity.ChatPet;
-import net.monkeystudio.chatrbtw.entity.WxFan;
 import net.monkeystudio.chatrbtw.mapper.ChatPetMapper;
 import net.monkeystudio.chatrbtw.service.ChatPetService;
 import net.monkeystudio.chatrbtw.service.WxFanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 
 /**
@@ -37,36 +28,6 @@ public class BackdoorController {
     @Autowired
     private ChatPetMapper chatPetMapper;
 
-
-    /**
-     * @param request
-     * @param response
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping(value = "/revise-coin", method = RequestMethod.GET)
-    public RespBase reviseCoin(HttpServletRequest request, HttpServletResponse response){
-        /*
-        1,获取之前的所有数据
-        * */
-
-        return respHelper.ok();
-    }
-
-    /**
-     * @param request
-     * @param response
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping(value = "/revise-exp", method = RequestMethod.GET)
-    public RespBase reviseExp(HttpServletRequest request, HttpServletResponse response){
-        /*
-        1,获取之前的所有数据
-        * */
-
-        return respHelper.ok();
-    }
 
     /**
      * @param request
